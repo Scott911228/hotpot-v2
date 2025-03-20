c4e69d512d076ca3f857e69e882fadc6005ec3b1
rick38621 <rick3862100@gmail.com>
2025-03-20 19:41:40 +0800

    勝利動畫嘗試套用

--- Assets/01-Scripts/WinAnimControl.cs
+++ Assets/01-Scripts/WinAnimControl.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
 using TMPro;
 using DG.Tweening;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
-public class PopUpText : MonoBehaviour
+public class WinAnimControl : MonoBehaviour
 {
     public TextMeshProUGUI levelClearText;
+    public GameObject achivment;
+    public string loadSceneName;
 
     void Start()
     {
-        levelClearText.transform.localScale = Vector3.zero;
-        levelClearText.transform.DOScale(1, 1.0f).SetEase(Ease.OutBack);
+        PlayWinAnim();
     }
+
+    void PlayWinAnim()
+    {
+        levelClearText.transform.localScale = Vector3.zero;
+        achivment.transform.localScale = Vector3.zero;
+        levelClearText.transform.DOScale(1, 1.0f).SetEase(Ease.OutBack).OnComplete(() =>
+        {
+            achivment.transform.DOScale(1, 1.0f).SetEase(Ease.OutExpo).OnComplete(() =>
+            {
+                DOVirtual.DelayedCall(2.0f, () =>
+                {
+                    SceneManager.LoadScene(loadSceneName);
+                });
+            });
+        });
+    }
+
+
 }
 
