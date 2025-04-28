16ff63b3ac58fb5e3afb94c53dcf94d5fbcbdd14
siori0714 <xia0714p@gmail.com>
2025-04-29 07:29:14 +0800

    fix compile

--- Assets/01-Scripts/WinAnimControl.cs
+++ Assets/01-Scripts/WinAnimControl.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
-using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 
 public class WinAnimControl : MonoBehaviour
