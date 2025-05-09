b7e7ef6436a3dad788df9c36974a657784e4c630
siori0714 <xia0714p@gmail.com>
2025-05-09 09:14:33 +0800

    修復重複切換火力可以無限增加攻擊力的bug

--- Assets/01-Scripts/Tutorial/HeatControl.cs
+++ Assets/01-Scripts/Tutorial/HeatControl.cs
@@ -14,6 +14,7 @@ public class HeatControl : MonoBehaviour
     public void ChangeHeat()
     {
         HeatLevel++;
+        HeatLevel %= 3;
         switch (HeatLevel % 3)
         {
             case 0:
