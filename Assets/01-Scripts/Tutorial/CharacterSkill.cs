a35d084361bd4fd5edfc4dd2619041d0e1857714
siori0714 <xia0714p@gmail.com>
2025-05-09 08:38:23 +0800

    新增蛋餃技能

--- Assets/01-Scripts/Tutorial/CharacterSkill.cs
+++ Assets/01-Scripts/Tutorial/CharacterSkill.cs
@@ -7,6 +7,7 @@ public class CharacterSkill : MonoBehaviour
     public float targetMP = 100;
     public float currentMP = 0;
     public float gainMPspeed = 0.1f;
+    public int skillID;
     public string skillName;
     [TextArea]
     public string skillDesc;
@@ -20,7 +21,7 @@ public class CharacterSkill : MonoBehaviour
     private Coroutine currentBoostCoroutine;
     void Start()
     {
-        if(!hasSkill) return;
+        if (!hasSkill) return;
         animator = GetComponent<Animator>();
         originalMultiplier = animator.GetFloat("runMultiplier");
         currentMP = 0; // 初始化 MP
@@ -48,14 +49,45 @@ public class CharacterSkill : MonoBehaviour
         isSkillTriggering = true;
         currentMP = 0;
         HealthBar.UpdateHealthBar(targetMP, currentMP);
-        BoostRunMultiplier(2f, 10f);
         GameObject.Find("GameControl").GetComponent<GameManager>().DisplayFloatingText(
         gameObject,
         "發動技能 !",
         40,
         new Color(0.09411757f, 0.05882348f, 0.05882348f, 0.8705882f));
+        if (skillID == 0) BoostRunMultiplier(2f, 10f);
+        else if (skillID == 1) Invincible(8f);
+    }
+    public void Invincible(float multiplier = 1.5f, float duration = 3f)
+    {
+        if (currentBoostCoroutine != null)
+        {
+            StopCoroutine(currentBoostCoroutine);
+        }
+        currentBoostCoroutine = StartCoroutine(RunInvincibleRoutine(duration));
     }
 
+    private IEnumerator RunInvincibleRoutine(float duration)
+    {
+        float elapsed = 0f;
+        GetComponent<CharacterHP>().AddProtect(0.0f, duration);
+        // 升速階段：從原值 → 目標值
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        // 回復階段（你可以選擇立即回復或平滑回復）
+        float returnDuration = 0.5f;
+        float returnElapsed = 0f;
+
+        while (returnElapsed < returnDuration)
+        {
+            returnElapsed += Time.deltaTime;
+            yield return null;
+        }
+        isSkillTriggering = false;
+        currentBoostCoroutine = null;
+    }
     public void BoostRunMultiplier(float multiplier = 1.5f, float duration = 3f)
     {
         if (currentBoostCoroutine != null)
